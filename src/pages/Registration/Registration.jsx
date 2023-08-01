02de526700c535d60a0104b3d3e46953d751c1a9
Md Samaul <mdsamal843@gmail.com>
2023-08-02 00:25:49 +0600

    test position

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -23,8 +23,8 @@ const Registration = () => {
     return (
 
         <div className='my-14 relative h-[100vh]'>
-            <div className='fixed transition duration-500 bg-red-600  top-0 left-0   right-0'>
-                <div className='flex mt-3 items-center w-full bg-red-600 text-base font-medium  py-2 text-white text-start gap-4 pl-1'>
+            <div className='fixed transition duration-500 bg-red-600  top-0 left-0  right-0'>
+                <div className='flex mt-6 items-center w-full bg-red-600 text-base font-medium  pb-2 text-white text-start gap-4 pl-1'>
                     <AiOutlineArrowLeft />
                     <button>Donor Registration</button>
                 </div>
@@ -44,7 +44,7 @@ const Registration = () => {
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus. Voluptates aut quis error earum ipsa officiis aliquam consequatur laudantium dolores atque magnam laborum exercitationem deserunt,
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, possimus. Voluptates aut quis error earum ipsa officiis aliquam consequatur laudantium dolores atque magnam laborum exercitationem deserunt,
                 </div>
-                <div className='fixed inset-x-0 bottom-0 bg-red-700 w-full h-14'>
+                <div className='fixed inset-x-0 bottom-0 bg-red-700 w-full h-11'>
                     <Button type="submit" btnText={'Register'} ></Button>
 
                 </div>
